4d22976cc4246192b71ffd1d88eb629c5965b792
mouseman <126301440+prabhav3@users.noreply.github.com>
2024-04-17 01:00:06 +0000

    Added basic HTML and some CSS and connected them

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 var path = require('path');
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname, ))
+  res.sendFile(path.join(__dirname, "..", "/public/html/index.html"))
 });
 
 module.exports = router;
